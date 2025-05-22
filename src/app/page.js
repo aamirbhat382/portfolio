eec99dd360d2c04cff26980a523c721c6626e339
Aamir Hussain <aamirbhat382@gmail.com>
2025-05-22 16:20:25 +0530

    Lets See How Meny Times She is visiting Her PAge

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
-import ProjectsSection from "./components/ProjectsSection";
+import ProjectsSection from "./components/ProjectsSection";x
 import ContactSection from "./components/ContactSection";
 
 const projects = [
